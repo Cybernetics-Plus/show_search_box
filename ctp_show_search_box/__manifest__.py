36162e164144a0861d89d423210b82e312f04124
BOMEN <bomen21x@gmail.com>
2022-11-21 18:10:48 +0700

    #

--- ctp_show_search_box/__manifest__.py
+++ ctp_show_search_box/__manifest__.py
@@ -7,30 +7,32 @@
 ###################################################################################
 
 {
-    'name': 'Show Home Backend Search Box',
-    'version': '15.0.1.0.1',
-    'summary': """ 
+    "name": "Show Home Backend Search Box",
+    "version": "15.0.1.0.1",
+    "summary": """ 
             Cybernetics Plus Tools Show Home Backend Search Box
             .""",
-    'description': """ 
+    "description": """ 
             Cybernetics Plus Tools Show Home Backend Search Box
             .""",
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.png'],
-    'category': 'Extra Tools',
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics Plus",
+    "company": "Cybernetics Plus Co., Ltd.",
+    "maintainer": "Cybernetics Plus Co., Ltd.",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.png"],
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
     ],
-    'depends': ['base'],
-    'assets': {
-        'web.assets_backend': [
-            'ctp_show_search_box/static/src/js/search_box.js',
+    "depends": ["base"],
+    "assets": {
+        "web.assets_backend": [
+            "ctp_show_search_box/static/src/js/search_box.js",
         ],
     },
 }
